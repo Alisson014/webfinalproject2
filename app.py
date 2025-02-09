3c5b05cec58506fd2df814f4436ec1f9af472e78
José Alisson <josealissondiasdacostaf10@gmail.com>
2025-02-09 12:03:46 -0300

    AdicionarComentario

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
+from flask_session import Session
 import sqlite3
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+def CheckUser(email):
+    con = sqlite3.connect('banco.db')
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM usuarios")
+    allusers = cursor.fetchall()
+    isUsed = False
+    for i in allusers:
+        # print(i)
+        if i[2] == email:
+            isUsed = True
+
+    con.close()
+    # print("Check user on")
+    return isUsed
+
+def getComents():
+    con = sqlite3.connect('banco.db')
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM comentarios;")
+    comentarios = cursor.fetchall()
+    con.close()
+    comentarios.reverse()
+    
+    return comentarios
+
+# print(CheckUser("jose@gmail.com"))
 
 @app.route('/')
 @app.route('/index')
 def index():
+    if not(session.get("nome")):
+        session["nome"] = None
+        session["email"] = None
+        session["senha"] = None
+
     return render_template("index.html")
 
 @app.route('/login', methods=["POST"])
@@ -15,9 +52,9 @@ def login():
 
 @app.route('/login/entrar', methods=["POST"])
 def Entrar():
-    nome = request.form.get('User_nome')
-    email = request.form.get('User_email')
-    senha = request.form.get('User_senha')
+    session["nome"] = request.form.get('User_nome')
+    session["email"] = request.form.get('User_email')
+    session["senha"] = request.form.get('User_senha')
 
     con = sqlite3.connect('banco.db')
     cursor = con.cursor()
@@ -25,88 +62,83 @@ def Entrar():
     allusers = cursor.fetchall()
     for i in allusers:
         exist = False
-        print(i[1:], [nome, email, senha])
-        if i[1:] == (nome, email, senha):
+        # print(i[1:], [nome, email, senha])
+        if i[1:] == (session.get("nome"), session.get("email"), session.get("senha")):
             exist = True
-
+            break
+    
+    con.close()
+    # comentarios = "hlylltt"
     if exist:
-        return render_template("home.html", nome = nome, email = email, senha = senha)
+        comentarios = getComents()
+
+        return render_template("home.html", comentarios = comentarios)
     else: 
         return render_template("login.html", isUsed = True, message = "Dados incorretos")
 
 
 @app.route('/login/cadastrar', methods=["POST"])
 def cadastrar():
-    nome = request.form.get('nome')
-    email = request.form.get('email')
-    senha = request.form.get('senha')
-
+    session["nome"] = request.form.get('User_nome')
+    session["email"] = request.form.get('User_email')
+    session["senha"] = request.form.get('User_senha')
+    # nome,email,senha = "123"
     # print(nome, email, senha)
 
     con = sqlite3.connect('banco.db')
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM usuarios")
-    allusers = cursor.fetchall()
-    isUsed = False
-    for i in allusers:
-        if i[2] == email:
-            isUsed = True
+    comentarios = getComents()
+    
+    isUsed = CheckUser(session["email"])
     
     if not(isUsed):
-        cursor.execute('INSERT INTO usuarios(nome, email, senha) VALUES(?, ?, ?)', (nome, email, senha))
+        cursor.execute('INSERT INTO usuarios(nome, email, senha) VALUES(?, ?, ?)', (session.get("nome"), session.get("email"), session.get("senha")))
         con.commit()
         con.close()
 
-        con2 = sqlite3.connect("banco.db")
-        cursor2 = con.cursor()
-        cursor2.execute("SELECT * FROM comentarios")
-        allcoments = cursor2.fetchall()
-        con2.close()
-        print("\n")
-        print("\n")
-        print("\n")
-        print("\n")
-        print(allcoments)
-        print("\n")
-        print("\n")
-        print("\n")
-        print("\n")
-        print("\n")
-
-        return render_template("home.html", nome = nome, email = email, senha = senha, comentarios = allcoments)
+        return render_template("home.html", comentarios = comentarios)
 
     else: 
+        con.close()
         return render_template("login.html", isUsed = isUsed, message = "Está conta já está cadastrada")
 
+@app.route('/comentar', methods=["POST"])
+def comentar():
+    nome = request.form.get("Coment_nome")
+    palavra = request.form.get("Coment_palavra")
+    comentario = request.form.get("comentario")
+
+    nomeU = request.form.get('User_nome')
+    emailU = request.form.get('User_email')
+    senhaU = request.form.get('User_senha')
+
+    con = sqlite3.connect('banco.db')
+    cursor = con.cursor()
+    cursor.execute("INSERT INTO comentarios(assunto, nome, comentario) VALUES(?,?,?)", (palavra, nome, comentario))
+    con.commit()
+    con.close()
+    
+    comentarios = getComents()
+    
+
+    return render_template("home.html", comentarios = comentarios)
 
 @app.route('/logout', methods=["POST"])
 def Logout():
+    session["nome"] = None
+    session["email"] = None
+    session["senha"] = None
+
     return redirect(url_for("index"))
 
 
 
 @app.route('/Home', methods=["GET"])
 def Home():
-    # con = sqlite3.connect('banco.db')
-    # cursor = con.cursor()
-    # cursor.execute("SELECT * FROM usuarios;")
-    # allusers = cursor.fetchall()
-
-    # for i in allusers:
-    #     user = []
-    #     if i == listUser:
-    #         user = listUser.copy()
-
-    # for i in allusers:
-    #     print(i[1:])
-
-        # if len(user) == len(listUser):
-        #     return render_template("home.html", user = user)
-        # else: 
-        #     return "<h1>Erro, usuário inesistente</h1>"
+    
 
     return render_template("home.html")
         
 
 
-    cursor.close()
+    # cursor.close()
